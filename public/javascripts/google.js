735c5c24e323d2551423c1895cdc8c7624771578
Ivan Elí Du Teitsson <ivansc91@gmail.com>
2015-12-12 05:40:37 +0000

    s

--- public/javascripts/google.js
+++ public/javascripts/google.js
@@ -1,14 +1,34 @@
-    
-    var map;
+var latitude;
+var longitude;
+var map;
 
-    function initialize() {
-        var mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById('map'),
-        mapOptions);
+function initMap() {
+    var destLatitude = document.querySelector('#map').attributes.latitude.value;
+    var destLongitude = document.querySelector('#map').attributes.longitude.value;
+    destLatitude = parseFloat(destLatitude);
+    destLongitude = parseFloat(destLongitude);
+
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(latitude, longitude),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map'),
+    mapOptions);
+}
+
+function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else { 
+            console.log("Geolocation is not supported by this browser.");
+        }
     }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+    function showPosition(position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        initMap();
+    }
+
+google.maps.event.addDomListener(window, 'load', getLocation);
