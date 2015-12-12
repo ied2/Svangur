42e0095302d8e3de88a7adf4dbe50cf8dd4c81e7
Ivan Elí Du Teitsson <ivansc91@gmail.com>
2015-12-12 05:43:01 +0000

    d

--- public/javascripts/google.js
+++ public/javascripts/google.js
@@ -10,25 +10,25 @@ function initMap() {
 
     var mapOptions = {
         zoom: 8,
-        center: new google.maps.LatLng(latitude, longitude),
+        center: new google.maps.LatLng(64.1532475, -22.0068169),
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById('map'),
     mapOptions);
 }
 
-function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else { 
-            console.log("Geolocation is not supported by this browser.");
-        }
-    }
+// function getLocation() {
+//         if (navigator.geolocation) {
+//             navigator.geolocation.getCurrentPosition(showPosition);
+//         } else { 
+//             console.log("Geolocation is not supported by this browser.");
+//         }
+//     }
 
-    function showPosition(position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-        initMap();
-    }
+//     function showPosition(position) {
+//         latitude = position.coords.latitude;
+//         longitude = position.coords.longitude;
+//         initMap();
+//     }
 
-google.maps.event.addDomListener(window, 'load', getLocation);
+google.maps.event.addDomListener(window, 'load', initMap);
