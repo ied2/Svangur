75470f9054cc08eba309db46f11708f861ce597a
Ivan Elí Du Teitsson <ivansc91@gmail.com>
2015-12-15 01:02:42 +0000

    Navbar update for mobile

--- public/javascripts/simple-sidebar.js
+++ public/javascripts/simple-sidebar.js
@@ -8,6 +8,23 @@ var sidebar = (function() {
 		wrapper = document.querySelector("#wrapper");
 		sidebar = document.querySelector("#sidebar-wrapper");
 
+		slideOptions = document.querySelector(".slideOptions");
+
+		slideOptions.addEventListener("click", function (e) {
+			if(slide.className === "btn btn-default btn-sq-lg btn-xl hidden-xs toggled") {
+				slide.className = "btn btn-default btn-sq-lg btn-xl hidden-xs";
+				slide.textContent = ">";
+				wrapper.className = "";
+			}
+			else {
+				slide.className += " toggled";
+				slide.textContent = "<";
+				wrapper.className = "toggled";
+			}
+
+		});
+
+
 		//sidebar.addEventListener("click", function (e) {
     //  		if(wrapper.className === "toggled") {
     //  			wrapper.className = "";
@@ -22,8 +39,8 @@ var sidebar = (function() {
 		iclass = document.querySelector("#iclass");
 
 		slide.addEventListener("click", function (e) {
-			if(slide.className === "btn btn-default btn-sq-lg btn-xl toggled") {
-				slide.className = "btn btn-default btn-sq-lg btn-xl";
+			if(slide.className === "btn btn-default btn-sq-lg btn-xl hidden-xs toggled") {
+				slide.className = "btn btn-default btn-sq-lg btn-xl hidden-xs";
 				slide.textContent = ">";
 				wrapper.className = "";
 			}
